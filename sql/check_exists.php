bcb4b35de8089507ed2e944a74620cb0b8cae3b4
chevaro1 <45008651+chevaro1@users.noreply.github.com>
2021-01-05 00:10:45 +0000

    Update check_exists.php

--- sql/check_exists.php
+++ sql/check_exists.php
@@ -13,8 +13,17 @@ $start = $_GET['start'];
 #$date = "2020-12-24";
 #echo $date;
 
-#echo "gor date";
-require_once 'config.php';
+$server = "eu-cdbr-west-03.cleardb.net";
+$username = "b8e1af19b2b19f";
+$password = "1e770fa5";
+$db = "heroku_02d9a109da8ad10";
+
+$link = new mysqli($server, $username, $password, $db);
+
+// Check connection
+if (!$link) {
+  die("Connection failed: " . mysqli_connect_error());
+}
 #echo "got config";
 
 $sql = "SELECT COUNT(location) as total FROM `bookings` WHERE date = '$date' AND location = '$name' AND barn = '$barn' AND start_time = '$start'";
