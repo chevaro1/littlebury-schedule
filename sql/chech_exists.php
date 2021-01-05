3e8bdc2c8d5ccb2673c296d05f2ab4269e9a2318
chevaro1 <45008651+chevaro1@users.noreply.github.com>
2021-01-05 00:09:16 +0000

    Create chech_exists.php

---
+++ sql/chech_exists.php
@@ -0,0 +1,43 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+#echo "entered get schedule";
+$date = $_GET['date'];
+$barn = $_GET['barn'];
+$name = $_GET['name'];
+$start = $_GET['start'];
+#$date = "2020-12-24";
+#echo $date;
+
+#echo "gor date";
+require_once 'config.php';
+#echo "got config";
+
+$sql = "SELECT COUNT(location) as total FROM `bookings` WHERE date = '$date' AND location = '$name' AND barn = '$barn' AND start_time = '$start'";
+#echo $sql;
+
+$result = mysqli_query($link, $sql);
+
+if (!$result) {
+    echo "Connection failed: ". mysqli_connect_error();
+}
+
+$arr = [];
+$count = 0;
+#echo "result receivefd";
+if (mysqli_num_rows($result) > 0){
+    while($row = mysqli_fetch_assoc($result)){
+              
+        #array_push($arr, $row); 
+        #$count += 1;
+        $arr[] = $row;
+    }
+}
+#echo $count;
+
+$jsondata = json_encode($arr);
+echo $jsondata;
