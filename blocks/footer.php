da50cfad61d79c192430665e24adf42a168c9eb4
william <w.galvin.info@gmail.com>
2021-01-22 10:14:43 +0000

    connected google adsense

--- blocks/footer.php
+++ blocks/footer.php
@@ -3,7 +3,7 @@
   <h3>Produced By William Galvin</h3>
   <p>Any suggestions are welcome ;)</a></p>
   <div style="position:relative;bottom:55px;" class="w3-tooltip w3-right">
-    <span class="w3-text w3-theme-light w3-padding">Go To Top</span>    
+    <span class="w3-text w3-theme-light w3-padding">Go To Top</span> 
     <a class="w3-text-white" href="#myHeader"><span class="w3-xlarge">
     <i class="fa fa-chevron-circle-up"></i></span></a>
   </div>
@@ -18,3 +18,4 @@
 
   gtag('config', 'G-85GZBLTQ31');
 </script>
+<script data-ad-client="ca-pub-9105329071156774" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
